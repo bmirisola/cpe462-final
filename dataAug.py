0d7390b800a15b63eb5094d9b063e064a0665ee6
Riya Shrestha <rshrest2@stevens.edu>
2021-12-07 16:40:06 -0500

    rotates an image in 3 angles and saves it

--- dataAug.py
+++ dataAug.py
@@ -1,13 +1,15 @@
 import cv2
 import imutils
-key = cv2. waitKey(1)
-image = cv2.imread(r"saved_img.jpg")
-for i in range(3):
-    count = i
-    Rotated1_image = imutils.rotate(image, angle=i*45)
-    cv2.imwrite(filename='Rotate.jpg', img=Rotated1_image)
+#import os
 
-cv2.imshow("Rotated1", Rotated1_image)
+
+key = cv2. waitKey(1)
+image = cv2.imread("/Users/riyashrestha/cpe462-final/saved_img.jpg")
+for i in range(0, 4):
+    Rotated_image = imutils.rotate(image, i*45)
+    cv2.imwrite("f{}.jpeg".format(i), img=Rotated_image)
+
+cv2.imshow("Rotated", Rotated_image)
 cv2.waitKey(0)
 
 if key == ord('s'):
