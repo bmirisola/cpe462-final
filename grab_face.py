026026d482b3d30a46517b4e5e25f38a4f1cbbca
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-07 19:13:27 -0500

    cleaned up grab_face.py

--- grab_face.py
+++ grab_face.py
@@ -1,24 +1,17 @@
 import cv2
 
-key = cv2. waitKey(1)
 webcam = cv2.VideoCapture(0)
+
 while True:
-    try:
         check, frame = webcam.read()
         cv2.imshow("Capturing", frame)
-        if key == ord('s'):
+        if cv2.waitKey(1) & 0xFF == ord('s'):
             cv2.imwrite(filename='saved_img.jpg', img=frame)
-            webcam.release()
-            cv2.destroyAllWindows()
             print("Processing image...")
-            img_ = cv2.imread('saved_img.jpg', cv2.IMREAD_ANYCOLOR)
             print("Image saved!")
             break
+        elif cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    except(KeyboardInterrupt):
-        print("Turning off camera.")
-        webcam.release()
-        print("Camera off.")
-        print("Program ended.")
-        cv2.destroyAllWindows()
-        break
+webcam.release()
+cv2.destroyAllWindows()
