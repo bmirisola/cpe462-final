e5d25fd10420b633f30475876cb9623b8186d09a
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-09 03:14:37 -0500

    saved pic to person directory

--- grab_face.py
+++ grab_face.py
@@ -2,6 +2,7 @@ import os
 import cv2
 
 webcam = cv2.VideoCapture(0)
+path_img = os.path.abspath("saved_img.jpg")
 
 while True:
         check, frame = webcam.read()
@@ -9,10 +10,11 @@ while True:
         if cv2.waitKey(1) & 0xFF == ord('s'):
             try:
                 os.mkdir("data")
+                os.mkdir("data/person")
             except FileExistsError:
                 pass
             cwd = os.getcwd()
-            cv2.imwrite(filename="{}/{}/saved_img.jpg".format(cwd,"data"), img=frame)
+            cv2.imwrite(filename="{}/{}/saved_img.jpg".format(cwd,"data/person"), img=frame)
             print("Processing image...")
             print("Image saved!")
             break
