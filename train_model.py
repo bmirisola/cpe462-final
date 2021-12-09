fca08c2c5b98559cd7a47086ad5b9f6baa4a756a
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-09 02:39:40 -0500

    trained a model

--- train_model.py
+++ train_model.py
@@ -1,12 +1,31 @@
-import numpy as np
-import os
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
-#import PIL
-#import PIL.Image
+import keras
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
-#import tensorflow_datasets as tfds
 
-print(tf.__version__)
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# Just train model for one person at the moment. Add a class for each person.
+# This should be automated in the future.
+num_of_classes = 1
+dataset = "data"
+validation_ratio = .15
+batch_size = 4
 
-#tf.device('/GPU:0')
+train_ds = tf.keras.utils.image_dataset_from_directory(dataset,validation_split=validation_ratio, subset="training",
+    seed=42,
+    label_mode='categorical',
+    batch_size=batch_size,
+    shuffle=True)
+
+base_model = ResNet50(weights='imagenet', include_top = False)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+global_avg_pooling = keras.layers.GlobalAveragePooling2D()(base_model.output)
+output = keras.layers.Dense(1, activation='sigmoid')(global_avg_pooling)
+
+face_classifier = keras.models.Model(inputs=base_model.input, outputs=output, name='ResNet50')
+
+face_classifier.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
+
+face_classifier.save("model/face_classifier.h5")
