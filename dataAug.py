254b6853ccd033c1908baf796b12affc4668f382
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-09 03:14:13 -0500

    added person path and stored as variable. Replaced cwd var

--- dataAug.py
+++ dataAug.py
@@ -6,23 +6,17 @@ import numpy as np
 
 
 # making an overall data dir and getting to the main img
-
-# I guarantee Riya didn't pull and is gonna text me saying "Benjamin it doesn't work!" after she pushes
-# Prove me wrong
 # When you read this please remember to always pull before you start working on a project.
 # It's a big time saver trust me (;
 
-cwd = os.getcwd()
 key = cv2.waitKey(1)
-image = cv2.imread("{}/{}/saved_img.jpg".format(cwd,"data"))
-
+path = os.path.abspath("data/person")
+image = cv2.imread("{}/saved_img.jpg".format(path))
 # rotating the img
-
-
 def rotate(image):
     for i in range(0, 4):
         Rotated_image = imutils.rotate(image, i * 45)
-        cv2.imwrite("{}/data/R{}.jpeg".format(cwd, i), img=Rotated_image)
+        cv2.imwrite("{}/R{}.jpeg".format(path, i), img=Rotated_image)
     if key == ord("s"):
         cv2.destroyAllWindows()
 
@@ -46,14 +40,12 @@ def add_noise(img):
 
 
 for j in range(0, 4):
-    cv2.imwrite("{}/data/sp{}.jpeg".format(cwd, j), img=add_noise(image))
-
-image = cv2.imread("{}/saved_img.jpg".format(cwd))
+    cv2.imwrite("{}/sp{}.jpeg".format(path, j), img=add_noise(image))
 
 # flip the img in 3 diff ways
 for f in range(-1, 2):
     fimage = cv2.flip(image, f)
-    cv2.imwrite("{}/data/f{}.jpeg".format(cwd, f), img=fimage)
+    cv2.imwrite("{}/f{}.jpeg".format(path, f), img=fimage)
 
 # resizing the img
 for s in range(1, 5):
@@ -61,7 +53,7 @@ for s in range(1, 5):
     height = int(image.shape[0] * 0.25 * s)
     dsize = (width, height)
     simage = cv2.resize(image, dsize)
-    cv2.imwrite("{}/data/rs{}.jpeg".format(cwd, s), img=simage)
+    cv2.imwrite("{}/rs{}.jpeg".format(path, s), img=simage)
 
 # translating the img
 height, width = image.shape[:2]
@@ -69,9 +61,9 @@ for t in range(1, 5):
     theight, twidth = height / t, width / t
     T = np.float32([[1, 0, twidth], [0, 1, theight]])
     img_translation = cv2.warpAffine(image, T, (width, height))
-    cv2.imwrite("{}/data/T{}.jpeg".format(cwd, T), img=img_translation)
+    cv2.imwrite("{}/T{}.jpeg".format(path, T), img=img_translation)
 
 # Cropping an image
 for c in range(1, 5):
     cropped_image = image[100 * c : 200 * c, 20 * c : 300 * c]
-    cv2.imwrite("{}/data/c{}.jpg".format(cwd, c), cropped_image)
+    cv2.imwrite("{}/c{}.jpg".format(path, c), cropped_image)
