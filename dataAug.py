b6ef3aee11c1a30c86dd3757d8086163d05327a0
Riya Shrestha <rshrest2@stevens.edu>
2021-12-11 19:17:44 -0500

    added more data

--- dataAug.py
+++ dataAug.py
@@ -12,8 +12,8 @@ path = os.path.abspath("data/person")
 image = cv2.imread("{}/saved_img.jpg".format(path))
 # rotating the img
 def rotate(image):
-    for i in range(0, 4):
-        Rotated_image = imutils.rotate(image, i * 45)
+    for i in range(1, 37):
+        Rotated_image = imutils.rotate(image, i * 10)
         cv2.imwrite("{}/R{}.jpeg".format(path, i), img=Rotated_image)
     if key == ord("s"):
         cv2.destroyAllWindows()
@@ -37,7 +37,7 @@ def add_noise(img):
     return img
 
 
-for j in range(0, 4):
+for j in range(0, 20):
     cv2.imwrite("{}/sp{}.jpeg".format(path, j), img=add_noise(image))
 
 image = cv2.imread("{}/saved_img.jpg".format(path))
@@ -47,29 +47,29 @@ for f in range(-1, 2):
     cv2.imwrite("{}/f{}.jpeg".format(path, f), img=fimage)
 
 # resizing the img
-for s in range(1, 5):
-    width = int(image.shape[1] * 0.25 * s)
-    height = int(image.shape[0] * 0.25 * s)
+for s in range(1, 10):
+    width = int(image.shape[1] * 0.1 * s)
+    height = int(image.shape[0] * 0.1 * s)
     dsize = (width, height)
     simage = cv2.resize(image, dsize)
     cv2.imwrite("{}/rs{}.jpeg".format(path, s), img=simage)
 
 # translating the img
 height, width = image.shape[:2]
-for t in range(1, 5):
+for t in range(4, 31):
     theight, twidth = height / t, width / t
     T = np.float32([[1, 0, twidth], [0, 1, theight]])
     img_translation = cv2.warpAffine(image, T, (width, height))
-    cv2.imwrite("{}/T{}.jpeg".format(path, T), img=img_translation)
+    cv2.imwrite("{}/T{}.jpeg".format(path, t), img=img_translation)
 
 # Cropping an image
-for c in range(1, 5):
-    cropped_image = image[20 * c : 90 * c, 20 * c : 70 * c]
-    cv2.imwrite("{}/c{}.jpg".format(path, c), cropped_image)
+for c in range(1, 10):
+    cropped_image = image[5 * c : 30 * c, 1 * c : 30 * c]
+    cv2.imwrite("{}/cr{}.jpg".format(path, c), cropped_image)
 
-
-for i in range(0, 10):
-    brightness = int((30 * i + 20))
+# changing brightness
+for i in range(0, 20):
+    brightness = int((7 * i + 20))
     if brightness > 0:
         shadow = brightness
         max = 255
@@ -81,8 +81,9 @@ for i in range(0, 10):
     cal = cv2.addWeighted(image, al_pha, image, 0, ga_mma)
     cv2.imwrite("{}/b{}.jpeg".format(path, i), img=cal)
 
-for i in range(1, 10):
-    contrast = int((10 * i + 7))
+# changing contrast
+for i in range(1, 11):
+    contrast = int((4 * i + 7))
     Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast))
     Gamma = 127 * (1 - Alpha)
     cal = cv2.addWeighted(image, Alpha, image, 0, Gamma)
