3ca7f03062f27691933835431a356eaad6cf88a6
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-16 03:19:11 -0500

    started training model with cpu

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import os.path
 import keras
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
@@ -8,26 +9,54 @@ import tensorflow as tf
 # This should be automated in the future.
 num_of_classes = 1
 dataset = os.path.join(os.getcwd(),"data")
-print(dataset)
 validation_ratio = .15
-batch_size = 4
+batch_size = 16
 
-train_ds = tf.keras.utils.image_dataset_from_directory(dataset,validation_split=validation_ratio, subset="training",
-    seed=42,
-    label_mode='categorical',
-    batch_size=batch_size,
-    shuffle=True)
+with tf.device('/CPU:0'):
 
-base_model = ResNet50(weights='imagenet', include_top = False)
+    train_ds = tf.keras.utils.image_dataset_from_directory(dataset,validation_split=validation_ratio, subset="training",
+        seed=42,
+        image_size =(276,276),
+        label_mode='categorical',
+        batch_size=batch_size,
+        shuffle=True)
 
-for layer in base_model.layers:
-    layer.trainable = False
+    validation_ds = tf.keras.utils.image_dataset_from_directory(
+        dataset,
+        validation_split=validation_ratio,
+        subset="validation",
+        seed=42,
 
-global_avg_pooling = keras.layers.GlobalAveragePooling2D()(base_model.output)
-output = keras.layers.Dense(1, activation='sigmoid')(global_avg_pooling)
+        batch_size=batch_size,
+        label_mode='categorical',
+        shuffle=True)
 
-face_classifier = keras.models.Model(inputs=base_model.input, outputs=output, name='ResNet50')
+    base_model = ResNet50(weights='imagenet', include_top = False)
 
-face_classifier.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
+    for layer in base_model.layers:
+        layer.trainable = False
 
-face_classifier.save("model/face_classifier.h5")
+    global_avg_pooling = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+    output = tf.keras.layers.Dense(1, activation='sigmoid')(global_avg_pooling)
+
+    face_classifier = keras.models.Model(inputs=base_model.input, outputs=output, name='ResNet50')
+
+    # ModelCheckpoint to save model in case of interrupting the learning process
+    checkpoint = ModelCheckpoint("models/face_classifier.h5",
+                                 monitor="val_loss",
+                                 mode="min",
+                                 save_best_only=True,
+                                 verbose=1)
+
+    # EarlyStopping to find best model with a large number of epochs
+    earlystop = EarlyStopping(monitor='val_loss',
+                              restore_best_weights=True,
+                              patience=5,  # number of epochs with no improvement after which training will be stopped
+                              verbose=1)
+
+    callbacks = [earlystop, checkpoint]
+
+    face_classifier.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.01), metrics=['accuracy'])
+
+    history = face_classifier.fit(train_ds, callbacks=callbacks, epochs=50, validation_data=validation_ds)
+    face_classifier.save("model/face_classifier.h5")
