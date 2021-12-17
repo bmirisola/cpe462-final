91763deaed050eefe5f58f756a939973c2e0e342
Benjamin Mirisola <bennymirisola@gmail.com>
2021-12-16 21:32:43 -0500

    properly trains model with correct shape

--- train_model.py
+++ train_model.py
@@ -9,6 +9,7 @@ from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
+import cv2
 
 # Just train model for one person at the moment. Add a class for each person.
 # This should be automated in the future.
@@ -16,27 +17,33 @@ num_of_classes = 1
 dataset = os.path.join(os.getcwd(),"data")
 validation_ratio = .15
 batch_size = 16
+img_path = "data/person/saved_img.jpg"
+img = cv2.imread(img_path)
+input_shape = img.shape
+img_height = img.shape[0]
+img_width = img.shape[1]
+print(input_shape)
 
 with tf.device('/CPU:0'):
 
     train_ds = tf.keras.utils.image_dataset_from_directory(dataset,validation_split=validation_ratio, subset="training",
         seed=42,
-        image_size =(276,276),
+        image_size=(img_height,img_width),
         label_mode='categorical',
         batch_size=batch_size,
         shuffle=True)
 
     validation_ds = tf.keras.utils.image_dataset_from_directory(
         dataset,
+        image_size=(img_height, img_width),
         validation_split=validation_ratio,
         subset="validation",
         seed=42,
-
         batch_size=batch_size,
         label_mode='categorical',
         shuffle=True)
 
-    base_model = ResNet50(weights='imagenet', include_top = False)
+    base_model = ResNet50(weights='imagenet', include_top = False, input_shape=input_shape)
 
     for layer in base_model.layers:
         layer.trainable = False
